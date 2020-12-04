7f15b2b9538128bcf0a81fe041b0fa4ea323b1a0
Christian Barlow <c.barlow108@gmail.com>
2020-12-04 15:23:26 -0600

    Add sqoop images, add backend routes and spark queries

--- covidAnalysisBackend/app.py
+++ covidAnalysisBackend/app.py
@@ -17,6 +17,19 @@ def load_USAData():
   csv_data = pd.read_csv("static/Input/byUSATotal.csv", sep=',')
   return csv_data
 
+def load_byHospitalizationTotal():
+    csv_data = pd.read_csv("static/Input/byHospitalizationTotal.csv", sep=',')
+    return csv_data
+
+def load_byDateDeaths():
+    csv_data = pd.read_csv("static/Input/byDateDeaths.csv", sep=',')
+    return csv_data
+
+def load_byLargestTestingIncrease():
+    csv_data = pd.read_csv("static/Input/byLargestTestingIncrease.csv", sep=',')
+    return csv_data
+
+
 @app.route('/api/byDeath')
 def byCountry():
     data = load_deathData()
@@ -30,6 +43,21 @@ def byMovie():
 @app.route('/api/byUSATotal')
 def byCharacter():
     data = load_USAData()
+    return data.to_json(orient='records')
+
+@app.route('/api/byHospitalizationTotal')
+def byHospitalizationTotal():
+    data = load_byHospitalizationTotal()
+    return data.to_json(orient='records')
+
+@app.route('/api/byDateDeaths')
+def byDateDeaths():
+    data = load_byDateDeaths()
+    return data.to_json(orient='records')
+
+@app.route('/api/byLargestTestingIncrease')
+def byLargestTestingIncrease():
+    data = load_byLargestTestingIncrease()
     return data.to_json(orient='records')
 
 @app.route('/')
--- covidAnalysisBackend/executeQuery.py
+++ covidAnalysisBackend/executeQuery.py
@@ -6,17 +6,17 @@ import requests
 import findspark
 findspark.init()
 from pyspark.sql import SparkSession
-from pyspark import SparkContext, SparkConf
-from pyspark.sql.functions import to_date
-from pyspark.sql.functions import col
-from pyspark.sql.functions import from_unixtime
-from pyspark.sql.functions import unix_timestamp
-from pyspark.sql.functions  import date_format
-from pyspark.sql.functions import regexp_replace
-from pyspark.sql.functions import regexp_extract
-from pyspark.sql.functions import split
-from pyspark.sql.functions import udf
-from pyspark.sql.types import *
+# from pyspark import SparkContext, SparkConf
+# from pyspark.sql.functions import to_date
+# from pyspark.sql.functions import col
+# from pyspark.sql.functions import from_unixtime
+# from pyspark.sql.functions import unix_timestamp
+# from pyspark.sql.functions  import date_format
+# from pyspark.sql.functions import regexp_replace
+# from pyspark.sql.functions import regexp_extract
+# from pyspark.sql.functions import split
+# from pyspark.sql.functions import udf
+# from pyspark.sql.types import *
 
 spark = SparkSession.builder.appName("Covid Analysis").getOrCreate()
 import pandas as pd
@@ -62,3 +62,22 @@ query5 = spark.sql("select state, max(positiveIncrease) as maxIncrease from covi
 query5.show()
 pd = query5.toPandas()
 pd.to_csv('static/Input/byMaxIncrease.csv', index=False)
+
+#Query 6: State with most hospitalizations
+query6 = df.groupBy("state").agg({"hospitalized" : "sum"}).withColumnRenamed("sum(hospitalized)", "totalHospitalizations").orderBy("totalHospitalizations", ascending=False)
+query6.show()
+pd = query6.toPandas()
+pd.to_csv('static/Input/byHospitalizationTotal.csv', index=False)
+
+#Query 7: Date with most deaths
+query7 = df.groupBy("date").agg({"death" : "sum"}).withColumnRenamed("sum(death)", "totalDeaths").orderBy("totalDeaths", ascending=False)
+query7.show()
+pd = query7.toPandas()
+pd.to_csv('static/Input/byDateDeaths.csv', index=False)
+
+#Query 8: States with largest increase in testing
+query8 = df.groupBy("state").agg({"totalTestResultsIncrease": "max"}).withColumnRenamed("max(totalTestResultsIncrease)", "maxTestIncrease").orderBy("maxTestIncrease", ascending=False)
+query8.show()
+pd = query8.toPandas()
+pd.to_csv('static/Input/byLargestTestingIncrease.csv', index=False)
+
